f2664ed27299bc7b3cff65742db9190488a266c0
tonylomaxx <tony.lomax@hekahappy.com>
2022-01-31 11:35:37 +0000

    Export classes not types

--- index.ts
+++ index.ts
@@ -1,3 +1,2 @@
-import { defaultModelCreation, createModel } from "./src/createModel";
-import type { MemoryModel, CustomModel } from "./src/createModel";
+import { defaultModelCreation, createModel, MemoryModel, CustomModel } from "./src/createModel";
 export { createModel, defaultModelCreation, MemoryModel, CustomModel };
